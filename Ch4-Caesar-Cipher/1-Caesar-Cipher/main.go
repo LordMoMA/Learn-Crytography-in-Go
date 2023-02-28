508e3791a81321fcda1a823a696251b73d89bc9c
David Lee <lihongyi0512@gmail.com>
2023-03-01 01:43:34 +0800

    “ch4”

--- Ch4-Caesar-Cipher/1-Caesar-Cipher/main.go
+++ Ch4-Caesar-Cipher/1-Caesar-Cipher/main.go
@@ -115,6 +115,9 @@ func crypt(text string, key int) string {
 
 }
 
+// since the alphabet is finite,
+// we need to use the modulo operator % to "wrap around" the index
+// if it exceeds the length of the alphabet.
 func getOffsetChar(c rune, offset int) string {
 	const alphabet = "abcdefghijklmnopqrstuvwxyz"
 	for i, curr := range alphabet {
@@ -146,3 +149,18 @@ func main() {
 	test("correcthorsebatterystaple", 16)
 	test("onetwothreefourfivesixseveneightnineten", 25)
 }
+
+/*
+Caesar Cipher Security
+The Caesar cipher is not secure in today's digital world.
+
+Assuming that an attacker knows or guesses that a Caesar cipher has been used, several key pieces of information are leaked immediately:
+
+The language and character set of the original plaintext
+The length of the original plaintext
+Frequency analysis can then be employed to quickly crack the code. On modern hardware, a Caesar cipher can be cracked instantly.
+
+For example, in English, the letter e is by far the most commonly used.
+If the attacker can find the most common letter in the ciphertext, then that letter very likely maps to the plaintext e.
+Because a Caesar cipher uses a fixed-length shift, the key is immediately discovered and the ciphertext can be compromised.
+*/
