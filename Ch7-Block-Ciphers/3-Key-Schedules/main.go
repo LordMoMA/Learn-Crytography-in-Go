391385dc63c7afd70eae5da645bf8b24bfdd0ee3
David Lee <lihongyi0512@gmail.com>
2023-03-03 23:23:03 +0800

    “ch7”

--- Ch7-Block-Ciphers/3-Key-Schedules/main.go
+++ Ch7-Block-Ciphers/3-Key-Schedules/main.go
@@ -33,3 +33,88 @@ masterKey     = 01101100 01110000...
 roundNumber   = 00000101
 roundKey      = 01101001 01110101...
 */
+
+package main
+
+import "fmt"
+
+/*
+
+this is wrong, and byte(roundNumber) is still wrong
+
+	func deriveRoundKey(masterKey [4]byte, roundNumber int) [4]byte {
+	roundKey := []byte{}
+	for b := range masterKey {
+		xor := b ^ roundNumber
+		roundKey = append(roundKey, xor)
+	}
+	return roundKey
+}
+*/
+
+func deriveRoundKey(masterKey [4]byte, roundNumber int) [4]byte {
+	var roundKey [4]byte
+	for i := 0; i < 4; i++ {
+		roundKey[i] = byte(roundNumber) ^ masterKey[i]
+	}
+	return roundKey
+}
+
+// don't touch below this line
+
+func test(masterKey [4]byte) {
+	fmt.Printf("Deriving round keys from master: %X...\n", masterKey)
+	for i := 1; i < 9; i++ {
+		roundKey := deriveRoundKey(masterKey, i)
+		fmt.Printf(" - Round key %v: %X\n", i, roundKey)
+	}
+	fmt.Println("========")
+}
+
+func main() {
+	test([4]byte{0xAA, 0xFF, 0x11, 0xBC})
+	test([4]byte{0xEB, 0xCD, 0x13, 0xFC})
+}
+
+/*
+
+Deriving round keys from master: AAFF11BC...
+
+ - Round key 1: ABFE10BD
+
+ - Round key 2: A8FD13BE
+
+ - Round key 3: A9FC12BF
+
+ - Round key 4: AEFB15B8
+
+ - Round key 5: AFFA14B9
+
+ - Round key 6: ACF917BA
+
+ - Round key 7: ADF816BB
+
+ - Round key 8: A2F719B4
+
+========
+
+Deriving round keys from master: EBCD13FC...
+
+ - Round key 1: EACC12FD
+
+ - Round key 2: E9CF11FE
+
+ - Round key 3: E8CE10FF
+
+ - Round key 4: EFC917F8
+
+ - Round key 5: EEC816F9
+
+ - Round key 6: EDCB15FA
+
+ - Round key 7: ECCA14FB
+
+ - Round key 8: E3C51BF4
+
+========
+*/
