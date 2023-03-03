19e1f75ffa241460d1825ecbd0341a5d31619e57
David Lee <lihongyi0512@gmail.com>
2023-03-04 01:02:46 +0800

    “ch8”

--- Ch8-DES/2-Data-Encryption-Standard/main.go
+++ Ch8-DES/2-Data-Encryption-Standard/main.go
@@ -30,3 +30,97 @@ Return any errors that occur.
 Tip
 Don't be afraid to debug your code by printing out the values of different variables. Just be sure to remove any debug code before submitting your answer.
 */
+
+package main
+
+import (
+	"bytes"
+	"crypto/cipher"
+	"crypto/des"
+	"errors"
+	"fmt"
+)
+
+func encrypt(key, plaintext []byte) ([]byte, error) {
+	block, err := des.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	if len(plaintext) < des.BlockSize {
+		return nil, errors.New("ciphertext too short")
+	}
+	iv := plaintext[:des.BlockSize]
+	plaintext = plaintext[des.BlockSize:]
+
+	if len(plaintext)%des.BlockSize != 0 {
+		padMsg(plaintext, des.BlockSize)
+	}
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(plaintext, plaintext)
+	return plaintext, nil
+
+}
+
+func padMsg(plaintext []byte, blockSize int) []byte {
+	for len(plaintext) < blockSize {
+		plaintext = append(plaintext, 0)
+	}
+	return plaintext
+}
+
+// don't touch below this line
+
+func decrypt(key, ciphertext []byte) (plaintext []byte, err error) {
+	block, err := des.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	if len(ciphertext) < des.BlockSize {
+		return nil, errors.New("ciphertext too short")
+	}
+	iv := ciphertext[:des.BlockSize]
+	ciphertext = ciphertext[des.BlockSize:]
+	if len(ciphertext)%des.BlockSize != 0 {
+		return nil, errors.New("ciphertext is not a multiple of the block size")
+	}
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(ciphertext, ciphertext)
+	return ciphertext, nil
+}
+
+func padWithZeros(block []byte, desiredSize int) []byte {
+	for len(block) < desiredSize {
+		block = append(block, 0)
+	}
+	return block
+}
+
+func test(key, plaintext []byte) {
+	ciphertext, err := encrypt(key, plaintext)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Encrypting '%v' with key '%v'...\n", string(plaintext), string(key))
+	decryptedText, err := decrypt(key, ciphertext)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	decryptedText = bytes.Trim(decryptedText, "\x00")
+	fmt.Printf("Decrypted: '%v'\n", string(decryptedText))
+	fmt.Println("========")
+}
+
+func main() {
+	test(
+		[]byte("12344321"),
+		[]byte("Today I met my crush, what a hunk"),
+	)
+
+	test(
+		[]byte("p@$$w0rd"),
+		[]byte("I hope my boyfriend never finds out about this"),
+	)
+}
