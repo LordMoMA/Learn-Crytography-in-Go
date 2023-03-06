52effb7ff205de4e55aec4b9d208bb9fc05bc0dc
David Lee <lihongyi0512@gmail.com>
2023-03-07 00:09:36 +0800

    “ch9”

--- Ch9-AES/2-Nonces/main.go
+++ Ch9-AES/2-Nonces/main.go
@@ -15,3 +15,48 @@ Example
 0b11000000
 Each nonce above has a strength of 256 because there are 256 possible combinations of bits that could exist in an 8-bit nonce.
 */
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// nonceStrength returns the number of bits of entropy in the nonce.
+func nonceStrength(nonce []byte) int {
+	return 2 ^ len(nonce)
+}
+
+// don't touch below this line
+
+func generateIV(length int) ([]byte, error) {
+	randomBytes := make([]byte, length)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return nil, err
+	}
+	return randomBytes, nil
+}
+
+func main() {
+	rand.Seed(0)
+	for i := 1; i < 6; i++ {
+		nonce, _ := generateIV(i)
+		nonceStr := nonceStrength(nonce)
+		fmt.Printf("A random nonce of %v bytes has strength of %v\n", i, nonceStr)
+	}
+}
+
+/*
+
+A random nonce of 1 bytes has strength of 3
+
+A random nonce of 2 bytes has strength of 0
+
+A random nonce of 3 bytes has strength of 1
+
+A random nonce of 4 bytes has strength of 6
+
+A random nonce of 5 bytes has strength of 7
+*/
