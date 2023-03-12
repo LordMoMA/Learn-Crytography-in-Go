ba54efee746507f1433d34d47b99e5a02c98658f
David Lee <lihongyi0512@gmail.com>
2023-03-13 00:57:02 +0800

    “ch12”

--- Ch12-Hash-Functions/main.go
+++ Ch12-Hash-Functions/main.go
@@ -23,6 +23,14 @@ A method on a pointer to a hasher. Uses h.Write() to write data to the hasher. I
 
 h.GetHex
 A method on a pointer to a hasher. Uses h.Sum() to get the hash value of the data written to the hasher. It should encode the hash value as a lowercase hex string and return it.
+
+////////////////////////////////////////////////////////////////
+
+In the SHA-256 hash we just implemented, there are an infinte number of possible inputs, but only 2^256 possible outputs. This means that there is a chance (albeit a very small one) that two different inputs can result in the same output.
+
+When this happens, it's called a collision.
+
+Because the number of potential inputs to a hash function is always infinite, every hash function has the potential for collisions. However, the larger the hash table (number of possible outputs) the less likely that two inputs will collide. Good hash functions, like SHA-256, make collisions extremely unlikely.
 */
 
 package main
@@ -73,3 +81,42 @@ func main() {
 	test([]string{"abercromni3", "f1tch", "123456", "abcdefg1234"})
 	test([]string{"IHeartNanciedrake", "m7B1rthd@y"})
 }
+
+/*
+
+Hashing vault of length 3...
+
+Adding 'password1' to vault hash...
+
+Adding 'password2' to vault hash...
+
+Adding 'password3' to vault hash...
+
+Vault hash: 2ccb27b6da88bb3a6906576751187be0957522c5d8429138936ffd4e461dac9e
+
+========
+
+Hashing vault of length 4...
+
+Adding 'abercromni3' to vault hash...
+
+Adding 'f1tch' to vault hash...
+
+Adding '123456' to vault hash...
+
+Adding 'abcdefg1234' to vault hash...
+
+Vault hash: a03ea2f828b1d33ac9cd3fa84a7df994536661aaabb8b4ff5af2a2a7c69ec31b
+
+========
+
+Hashing vault of length 2...
+
+Adding 'IHeartNanciedrake' to vault hash...
+
+Adding 'm7B1rthd@y' to vault hash...
+
+Vault hash: 7bf580ff478408fd5811ae7f6a32e2a13560840911a815baba72ebd5b9eca72c
+
+========
+*/
