a8706272edbcae6b36f14008252441509bed0348
David Lee <lihongyi0512@gmail.com>
2023-03-15 02:00:13 +0800

    “ch13”

--- Ch13-Digital-Signature/3-Toy-HMAC/main.go
+++ Ch13-Digital-Signature/3-Toy-HMAC/main.go
@@ -10,6 +10,22 @@ Complete the hmac function. It should:
 Split the key into two halves. The second half should be the larger half if key's length is odd
 Return the result of sha256(keyFirstHalf + sha256(keySecondHalf + message)) as a string in lowercase hex
 Cast strings directly to slices of bytes and don't use any delimiters when concatenating the data.
+
+MACs and JWTs
+You might be thinking that it's inconvenient to only be able to validate messages with people who share a copy of the secret key. However, this is how many web applications handle authentication, including Boot.dev!
+
+JWTs for user authentication
+A JWT, or JSON Web Token, is just an HMAC where the message data is a JSON object. For example:
+
+message = {"userID": "11be9160-2243-4449-934b-e8245fe2feb0"}
+hmac = sha256(key1 + sha256(k2 + message))
+
+jwt = message + "." + hmac
+JWTs that use HMACs are useful for authentication because it's the same server that's issuing and validating the tokens.
+
+jwt lifecycle
+
+I use the golang-jwt package in Go quite often, it's a great library for creating and validating JWTs.
 */
 
 package main
