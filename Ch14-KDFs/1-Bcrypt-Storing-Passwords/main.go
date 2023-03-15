037dfcb096ade0f52b5d28357e7a45505ad9890c
David Lee <lihongyi0512@gmail.com>
2023-03-15 21:12:57 +0800

    “ch14”

--- Ch14-KDFs/1-Bcrypt-Storing-Passwords/main.go
+++ Ch14-KDFs/1-Bcrypt-Storing-Passwords/main.go
@@ -41,6 +41,31 @@ Use the golang.org/x/crypto/bcrypt package to complete the hashPassword() and ch
 Use a cost factor of 13
 Docs for bcrypt.GenerateFromPassword
 Docs for bcrypt.CompareHashAndPassword
+
+Bcrypt Review
+Let's take a look at the output of the Bcrypt KDF that we just implemented:
+
+$2a$10$Z9FCUt.58BSe5RdBQmPhPOfMQO4WDb8LwlY11Oy0wJ8HNXbSiNYSa
+This isn't just a slow hash of the password input. It's actually a string with a format:
+
+$ VERSION $ COST_FACTOR $ SALT HASH
+To break it down:
+
+Version: $2a
+Cost Factor: 13
+Salt: Z9FCUt.58BSe5RdBQmPhPO (first 16-bytes of the last section)
+Hash: fMQO4WDb8LwlY11Oy0wJ8HNXbSiNYSa
+Version
+The version is a string that identifies the version of the Bcrypt algorithm used to generate the hash. This is stored in the output so that we can use different versions of the algorithm in the future without breaking old hashes.
+
+Cost factor
+The cost factor is a number that determines how many rounds of hashing are performed. The higher the cost factor, the more rounds of hashing are performed. This makes the hash slower to compute, but also makes it more secure. We need to store the cost factor in the output so that we can use it to check the hash later.
+
+Salt
+A salt is a random chunk of data added to a password before it is hashed so that its output hash will differ from the hash of the same password with a different salt. We'll talk about these in more detail soon. Again, we need it to be stored in the output so that we can use it to check the hash later.
+
+Hash
+The hash is the output of the Bcrypt hashing algorithm. It is the result of hashing the password with the salt and cost factor.
 */
 
 package main
