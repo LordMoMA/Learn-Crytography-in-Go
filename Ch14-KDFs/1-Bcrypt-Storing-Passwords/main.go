9507165c4f6cf29a8714f9f8d458579cb7910d5c
David Lee <lihongyi0512@gmail.com>
2023-03-15 20:55:08 +0800

    “ch14”

--- Ch14-KDFs/1-Bcrypt-Storing-Passwords/main.go
+++ Ch14-KDFs/1-Bcrypt-Storing-Passwords/main.go
@@ -23,4 +23,22 @@ Key whitening increases the security of block ciphers by combining portions of t
 
 4. Key Strengthening
 Less common than stretching, strengthing extends a key with a random salt, but then deletes the salt so it can’t be used again. This strengthens the resulting key, but comes at a strong convenience because even legitimate users won't be able to derive the original easily.
+
+Bcrypt - Storing Passwords
+KDFs are the best way to store passwords in web applications! As a back-end developer, this is critical to understand.
+
+Can I store passwords in plain text?
+Storing passwords in a database in plain text is a huge security risk. If someone gets access to your database, they can see all of your users' passwords.
+
+Can I hash passwords with SHA-256?
+No. SHA-256 is a hash function, but it's not a KDF. SHA-256 is a very fast hash function. Good KDFs like Bcrypt are designed to be slow. We'll talk more about why that's important soon.
+
+Assignment
+At Passly, we store passwords securely (it would be sad if we didn't). Each user has a master password that they use to log into their cloud account. That password is hashed with Bcrypt before being stored.
+
+Use the golang.org/x/crypto/bcrypt package to complete the hashPassword() and checkPasswordHash() functions. You do not need to modify the function signatures, just implement the Bcrypt library's API and do the []byte <-> string conversions.
+
+Use a cost factor of 13
+Docs for bcrypt.GenerateFromPassword
+Docs for bcrypt.CompareHashAndPassword
 */
