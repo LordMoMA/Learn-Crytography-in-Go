01d4835f9f661d70718547eea2bc4b467d3f77be
David Lee <lihongyi0512@gmail.com>
2023-03-15 21:46:28 +0800

    “ch14”

--- Ch14-KDFs/2-Salts/main.go
+++ Ch14-KDFs/2-Salts/main.go
@@ -27,6 +27,27 @@ Use crypto/rand to generate a random salt of the specified length. Use rand.Read
 
 hashPassword
 Append the salt directly to the end of the password, then hash it with SHA-256. Use crypto/sha256. Return the result of the hash.
+
+Why are KDFs slow?
+SHA-256 and MD5 are examples of fast hashes and are bad for storing password hashes. KDFs like Scrypt and Bcrypt are both extremely slow and are good for passwords.
+
+Always use slow KDFs for storing user passwords, never fast hashes.
+
+If an attacker gains access to a database of password hashes made with fast algorithms it is easy for them to "reverse" the hashes by guessing different inputs and seeing if the outputs match. For example, let's say the attacker finds the following entry in a database:
+
+Email	Password Hash
+bob@example.com	ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f
+They can try hashing common passwords like:
+
+password -> 0b14d501a594442a01c6859541bcb3e8164d183d32937b851835442f69d5c94e
+01011994 -> 6cf615d5bcaac778352a8f1f3360d23f02f34ec182e259897fd6ce485d7870d4
+password123 -> ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f
+By trying common passwords, they can quickly find a matching hash! In the example above, password123 matched.
+
+Now the attacker knows that bob@example.com is likely to use the password password123 on other sites and can go hack their accounts. This is only possible because the attacker can quickly compute many hashes per second and guess millions of potential passwords. If they were going through a login form, the server would probably block them after a few failed attempts.
+
+KDFs fix this
+In contrast, a slow hash function (KDF) will run so painfully slow that the attacker is unable to make enough guesses to be able to brute force their way to the plaintext of any passwords.
 */
 
 package main
